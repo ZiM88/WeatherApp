5f843d10680faa0236cc7c722d8e1ad118ea4c97
ZiM88 <renegadez@tut.by>
2022-01-17 15:05:23 +0300

    Creating project

---
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.weatherapp
+
+
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.location.LocationManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (!isLocationEnabled()) {
+            Toast.makeText(
+                this,
+                "Your location provider is turned off. Please turn it on.",
+                Toast.LENGTH_SHORT
+            ).show()
+
+            val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            startActivity(intent)
+        } else {
+            Toast.makeText(
+                this,
+                "Your location provider is already ON.",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    private fun isLocationEnabled(): Boolean {
+
+
+        val locationManager: LocationManager =
+            getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+}
+
